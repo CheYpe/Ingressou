dbee320b88240a44bb79eb27e83d354c11a1078a
joaoeymard <>
2017-12-17 01:53:09 -0300

    Get evento pelo note, melhorando o titulo e imagem da página

--- routes/evento.js
+++ routes/evento.js
@@ -1,12 +1,30 @@
 var express = require('express');
+var http = require('http');
 var router = express.Router();
 
 /* GET evento page. */
 router.get('/[a-zA-Z0-9_]+?', function(req, res, next) {
+
   var _link = req.path.replace('/','');
-  res.render('evento', {
-    titulo: _link+' - ingressou',
-    link: _link
+  http.get('http://18.231.16.165:3000/v1/evento/'+_link, (resp) => {
+
+    let data = '';
+
+    // A chunk of data has been recieved.
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+      data = JSON.parse(data)
+      data.titulo += ' - Ingressou'
+
+      res.render('evento', data);
+    });
+
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
   });
 });
 
--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { titulo: 'Ingressou - loja virtual de ingressos' });
+  res.render('index', { titulo: 'Ingressou - Loja Virtual de Ingressos', imagem: 'http://ingressou.com/images/perfil.jpg' });
 });
 
 /* GET login page. */
 router.get('/login', function(req, res, next) {
-  res.render('login', { titulo: 'Ingressou - Login' });
+  res.render('login', { titulo: 'Ingressou - Login', imagem: 'http://ingressou.com/images/perfil.jpg' });
 });
 
 /* GET login page. */
 router.get('/contato', function(req, res, next) {
-  res.render('contato', { titulo: 'Contato - Ingressou' });
+  res.render('contato', { titulo: 'Contato - Ingressou', imagem: 'http://ingressou.com/images/perfil.jpg' });
 });
 
 module.exports = router;
